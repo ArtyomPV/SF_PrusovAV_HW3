164b035a27c281f833e45d63887f62ae3f3512bb
Artyom <itcoding76@gmail.com>
2023-07-17 21:56:37 +0200

    добавлен метод update_rating

--- NewsPaperProject/NewsPaperApp/models.py
+++ NewsPaperProject/NewsPaperApp/models.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
-
-# Create your models here.
+from django.db.models import Sum
 
 
 class Author(models.Model):
     author_name = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        postRat = self.post_set.all().aggregate(posting=Sum('rating_post'))
+        prat = 0
+        prat += postRat.get('posting')
+        self.rating = prat
+        self.save()
 
 
 class Category(models.Model):
@@ -52,6 +58,9 @@ class Comment(models.Model):
     text_comment = models.TextField()
     data_comment_creation = models.DateTimeField(auto_now_add=True)
 
+    # def __str__(self):
+    #     return self.author_comment.username
+
     def like(self):
         self.rating_comment += 1
         self.save()
@@ -59,4 +68,3 @@ class Comment(models.Model):
     def dislike(self):
         self.rating_comment -= 1
         self.save()
-
